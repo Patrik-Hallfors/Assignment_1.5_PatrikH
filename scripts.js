f810ad43860efad4fbb7cb5ad1f3c31811d93b8d
Patrik-Hallfors <patrik.hallfors@gritacademy.se>
2024-12-18 15:43:32 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let age = 17;
+let vip = true;
+
+if ((vip = true)) alert("Welcome VIP Customer :)");
+else if (age <= 17) {
+  alert("Access Denied");
+} else age >= 18;
+alert("Access Granted");
+
